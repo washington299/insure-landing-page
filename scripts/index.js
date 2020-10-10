26cc8486bb439a6feb1fa5e3459baada8326f278
Washington <washingtoncampos9@gmail.com>
2020-10-10 20:14:29 -0300

    create menu mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
 const menu = document.querySelector('.header__icon-hamburger');
 menu.onclick = function() {
     const menu_src = menu.getAttribute('src');
+    const menu_mobile = document.querySelector('.header__list');
 
     if (menu_src === 'images/icon-hamburger.svg') {
         menu.setAttribute('src', 'images/icon-close.svg');
+        menu_mobile.style.display = 'flex';
     } else {
         menu.setAttribute('src', 'images/icon-hamburger.svg');
+        menu_mobile.style.display = 'none';
     }
 };
