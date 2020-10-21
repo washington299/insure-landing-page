8696e26376dfcfdd188bea2ced44b81e543a231b
Washington <washingtoncampos9@gmail.com>
2020-10-21 15:32:04 -0300

    refactor footer

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,17 @@ const menu = document.querySelector('.header__icon-hamburger');
 const changeMenuIcon = () => {
     const menu_src = menu.getAttribute('src');
     const menu_mobile = document.querySelector('.header__list');
+    const bg_pattern_nav_mob = document.querySelector('.header__bg-pattern-nav-mob');
     
     if (menu_src === 'images/icon-hamburger.svg') {
         menu.setAttribute('src', 'images/icon-close.svg');
         menu_mobile.style.display = 'flex';
+        bg_pattern_nav_mob.style.display = 'block';
         document.body.style.overflow = 'hidden';
     } else {
         menu.setAttribute('src', 'images/icon-hamburger.svg');
         menu_mobile.style.display = 'none';
+        bg_pattern_nav_mob.style.display = 'none';
         document.body.style.overflow = 'auto';
     }
 };
